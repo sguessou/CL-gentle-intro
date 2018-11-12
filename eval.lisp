559ec1bb702e367bd5f8091a0e5a1afd4e24e6d5
Saad Guessous <saad.guessous@leaddesk.com>
2018-11-12 14:05:51 +0200

    added Ex 3.11 + 3.12

--- eval.lisp
+++ eval.lisp
@@ -7,3 +7,13 @@
 (defun pythag (x y)
   (sqrt (+ (* x x) (* y y))))  
 
+;; Ex 3.11
+; Define a predicate called LONGER-THAN that takes two lists as input and returns T if the first list is longer than the second.
+(defun longer-than (a b)
+  (> (length a) (length b)))
+
+;; Ex 3.12
+; Write a function ADDLENGTH that takes a list as input and returns a new list with the length of the input added onto the front of it. If the input is (MOO GOO GAI PAN),
+; the output should be (4 MOO GOO GAI PAN).
+(defun addlength (my-list)
+  (cons (length my-list) my-list))
