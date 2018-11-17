42d2243dc5c32b1860222e40de463ab32cbec0f4
Saad Guessous <guessous_saad@hotmail.com>
2018-11-17 13:46:42 +0200

    added Ex 4.8

--- conditionals.lisp
+++ conditionals.lisp
@@ -39,3 +39,17 @@
   (cond ((< n 0) (* n -1))
         (t n)))
 
+;; Ex 4.8
+; Write EMPHASIZE3, which is like EMPHASIZE2 but adds the symbol VERY onto the list if it doesn't know how to emphasize it.
+; For example, EMPHASIZE3 of (LONG DAY) should produce (VERY LONG DAY).
+; What does EMPHASIZE3 of (VERY LONG DAY) produce?
+(defun emphasize2 (x)
+  (cond ((equal (first x) 'good) (cons 'great (rest x)))
+        ((equal (first x) 'bad (cons 'awful (rest x))))
+        (t x)))
+
+(defun emphasize3 (x)
+  (cond ((equal (first x) 'good) (cons 'great (rest x)))
+        ((equal (first x) 'bad) (cons 'awful (rest x)))
+        (t (cons 'very x))))
+
