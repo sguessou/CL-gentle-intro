9f0ff5c73b792e676685ca35750c8b19d3e63aa3
Saad Guessous <guessous_saad@hotmail.com>
2018-11-17 14:09:00 +0200

    added Ex 4.10

--- conditionals.lisp
+++ conditionals.lisp
@@ -63,3 +63,20 @@
 (defun make-odd-c (x)
   (cond ((oddp x) (+ x 1))
         (t x)))
+
+
+;; Ex.10
+; Write a function CONSTRAIN that takes three inputs called X, MAX, and MIN. If X is less than MIN, it should return MIN; if X is greater than MAX, it should return MAX.
+; Otherwise, since X is between MIN and MAX, it should return X. (CONSTRAIN 3 -50 50) should return 3. (CONSTRAIN 92 -50 50) should return 50.
+; Write one version using COND and another using nested IFs.
+(defun constrain-v1 (x max min)
+  (cond ((> max x) max)
+        ((< min x) min)
+        (t x)))
+
+(defun constrain-v2 (x max min)
+  (if (> max x)
+      max)
+  (if (< min x)
+      min)
+  x)
