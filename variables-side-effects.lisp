552016172c81451f3fb2466edc9c936bd564854c
Saad Guessous <guessous_saad@hotmail.com>
2018-11-22 22:10:46 +0200

    added Ex 5.6 a

--- variables-side-effects.lisp
+++ variables-side-effects.lisp
@@ -1,9 +1,9 @@
 ;;; Chapter 5 - Variables and Side Effects
-;; Exercises
+;;; Exercises
 
-;; Ex 5.1
-; Rewrite function POOR-STYLE to create a new local variable Q using LET, instead of using SETF to change P.
-; Call your new function GOOD-STYLE.
+;;; Ex 5.1
+;;; Rewrite function POOR-STYLE to create a new local variable Q using LET, instead of using SETF to change P.
+;;; Call your new function GOOD-STYLE.
 (defun poor-style (p)
   (setf p (+ p 5))
   (list 'result 'is p))
@@ -11,3 +11,11 @@
 (defun good-style (p)
   (let ((q (+ p 5)))
     (list 'result 'is q)))
+
+;;; Ex 5.6 a
+;;; Write a function THROW-DIE that returns a random number from 1 to 6, inclusive. Remember that (RANDOM 6) will pick numbers from 0 to 5.
+;;; THROW-DIE doesn't need any inputs, so its argument list should be NIL.
+(defun throw-die ()
+  "Returns a random number from 1 to 6."
+  (+ (random 6) 1))
+
