199f7ad82c8ac6078a807764a4b62160000ede48
Saad Guessous <guessous_saad@hotmail.com>
2018-11-25 19:35:47 +0200

    added file for list data structures + Ex 6.6

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # Common Lisp: A Gentle Introduction to Symbolic Computation
 
-## Working on the exercices included in the book: [Common Lisp: GISC](https://www.cs.cmu.edu/~dst/LispBook/)
+## My solutions for the exercices included in the book, you can get the book from the following link: [Common Lisp: GISC](https://www.cs.cmu.edu/~dst/LispBook/)
 
 * Chapter 1 - **Functions and Data**
 * Chapter 2 - **Lists**
 * Chapter 3 - **EVAL Notation**
 * Chapter 4 - **Conditionals**
 * Chapter 5 - **Variables and Side Effects**
+* Chapter 6 - **List Data Structures**
---
+++ list-data-structures.lisp
@@ -0,0 +1,17 @@
+;;; Chapter 6 - List Data Structures
+;;; Exercises
+
+;;; Ex 6.6
+;;; Use the LAST function to write a function called LAST-ELEMENT that returns the last element of a list instead of the last cons cell.
+;;; Write another version of LAST-ELEMENT using REVERSE instead of LAST. Write another version using NTH and LENGTH.
+(defun last-element (l)
+  "Returns last element of list version 1"
+  (car (last l)))
+
+(defun last-element (l)
+  "Returns last element of list version 2"
+  (car (reverse l)))
+
+(defun last-element (l)
+  "Returns last element of list version 3"
+  (nth (- (length l) 1) l))
