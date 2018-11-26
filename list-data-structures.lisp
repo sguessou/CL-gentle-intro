1107efc5eb0922f3c9c70c7cc3354b7e2d20583b
Saad Guessous <guessous_saad@hotmail.com>
2018-11-26 21:46:03 +0200

    added Ex 6.25

--- list-data-structures.lisp
+++ list-data-structures.lisp
@@ -84,3 +84,10 @@
 ;;; (Hint: If two sets are equal, then each is a subset of the other.)
 (defun set-equal (a b)
   (and (subsetp a b) (subsetp b a))) 
+
+;;; Ex 6.25
+;;; A set X is a proper subset of a set Y if X is a subset of Y but not equal to Y.
+;;; Thus, (A C) is a proper subset of (C A B). (A B C) is a subset of (C A B), but not a proper subset of it.
+;;; Write the PROPER-SUBSETP predicate, which returns T if its first input is a proper subset of its second input.
+(defun proper-subset (a b)
+  (and (subsetp a b) (not (subsetp b a)))) 
