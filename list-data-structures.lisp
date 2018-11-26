82e23c1a5910153721b19ae4c7dd9051dfcd2744
Saad Guessous <guessous_saad@hotmail.com>
2018-11-26 22:17:42 +0200

    added Ex 6.26

--- list-data-structures.lisp
+++ list-data-structures.lisp
@@ -91,3 +91,30 @@
 ;;; Write the PROPER-SUBSETP predicate, which returns T if its first input is a proper subset of its second input.
 (defun proper-subset (a b)
   (and (subsetp a b) (not (subsetp b a)))) 
+
+;;; Ex 6.26
+;;; We are going to write a program that compares the descriptions of two objects and tells how many features they have in common.
+;;; The descriptions will be represented as a list of features, with the symbol -VS- separating the first object from the second.
+;;; Thus, when given a list like (large red shiny cube -vs- small shiny red four-sided pyramid)
+;;; the program will respond with (2 COMMON FEATURES). We will compose this program from several small functions that you will write and test one at a time.
+;;; a.
+;;; Write a function RIGHT-SIDE that returns all the features to the right of the -VS- symbol. RIGHT-SIDE of the list shown above should return (SMALL SHINY RED FOUR-SIDED PYRAMID).
+;;; Test your function to make sure it works correctly.
+(defun right-side (o)
+  (cdr (member '-vs- o)))
+
+;;; b.
+;;; Write a function LEFT-SIDE that returns all the features to the left of the -VS-. You can't use the MEMBER trick directly for this one.
+(defun left-side (o)
+  (cdr (member '-vs- (reverse o))))
+
+;;; c.
+;;; Write a function COUNT-COMMON that returns the number of features the left and right sides of the input have in common.
+(defun count-common (o)
+  (length (intersection (left-side o) (right-side o))))
+
+;;; d.
+;;; Write the main function, COMPARE, that takes a list of features describing two objects, with a -VS- between them, and reports the number of features they have in common.
+;;; COMPARE should return a list of form (n COMMON FEATURES).
+(defun compare (o)
+  (cons (count-common o) '(common features)))
