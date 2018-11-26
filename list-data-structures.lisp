a6f77ecc2309c281513321a6edc1d69b9158f5aa
Saad Guessous <guessous_saad@hotmail.com>
2018-11-26 21:02:24 +0200

    added Ex 6.18

--- list-data-structures.lisp
+++ list-data-structures.lisp
@@ -65,3 +65,7 @@
              (not (member 'a sent))  
              (not (member 'an sent)))))
 
+;;; Write a function ADD-VOWELS that takes a set of letters as input and adds the vowels (A E I O U) to the set. 
+;;; For example, calling ADD-VOWELS on the set (X A E Z) should produce the set (X A E Z I O U), except that the exact order of the elements in the result is unimportant.
+(defun add-vowels (s)
+  (union s '(a e i o u)))
