e02b449ceb93dbee0f0c00878bdbe787b789e118
Saad Guessous <guessous_saad@hotmail.com>
2018-12-02 11:22:33 +0200

    added 7.3

--- applicative-programming.lisp
+++ applicative-programming.lisp
@@ -22,3 +22,7 @@ let the global variable DAILY-PLANET contain the following table:
        (white perry 355-16-7439 editor)))
 
 (mapcar #'third daily-planet)
+
+;;; Write an expression to apply the ZEROP predicate to each element of the list (2 0 3 4 0 -5 -6).
+;;; The answer you get should be a list of Ts and NILs.
+(mapcar #'zerop '(2 0 3 4 0 -5 -6))
