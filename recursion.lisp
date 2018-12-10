0aa3c1e47d3bb6daf4269e8b4944044dbc783433
Saad Guessous <guessous_saad@hotmail.com>
2018-12-10 22:05:45 +0200

    added Ex 8.18

--- recursion.lisp
+++ recursion.lisp
@@ -97,3 +97,10 @@
   (cond ((null x) nil)
         ((oddp (first x)) (first x))
         (t (find-first-odd (rest x)))))
+
+;;; Ex 8.18
+;;; Use single-test tail recursion to write LAST-ELEMENT, a function that returns the last element of a list.
+;;; LAST-ELEMENT should recursively travel down the list until it reaches the last cons cell (a cell whose cdr is an atom); then it should return the car of this cell.
+(defun last-element (x)
+  (cond ((atom (cdr x)) (car x))
+        (t (last-element (rest x)))))
