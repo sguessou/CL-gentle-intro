2bfb98e898d8894d083d7b174591463eba486ff0
Saad Guessous <guessous_saad@hotmail.com>
2018-12-15 00:18:58 +0200

    added Ex8.40

--- recursion.lisp
+++ recursion.lisp
@@ -258,3 +258,11 @@
 (defun count-atoms (l)
   (cond ((atom l) 1)
         (t (+ (count-atoms (car l)) (count-atoms (cdr l))))))
+
+;;; Ex 8.40
+;;; Write COUNT-CONS, a function that returns the number of cons cells in a tree.
+;;; (COUNT-CONS '(FOO)) should return one. (COUNT-CONS '(FOO BAR)) should return two. (COUNT-CONS '((FOO))) should also return two, since the list ((FOO)) requires two cons cells.
+;;; (COUNT-CONS 'FRED) should return zero.
+(defun count-cons (l)
+  (cond ((atom l) 0)
+        (t (+ 1 (count-cons (car l)) (count-cons (cdr l))))))
