36f4c2939a312eaf35a480d748f0df580c4fce24
Saad Guessous <guessous_saad@hotmail.com>
2018-12-26 22:44:18 +0200

    added ex9.2

--- input-output.lisp
+++ input-output.lisp
@@ -6,3 +6,17 @@
 ;;; The function should break up the quotation into several lines.
 (defun print-test ()
   (format t "There are old pilots,~&and there are bold pilots,~&but there are no old bold pilots.~& "))
+
+;;; Ex 9.2
+;;; Write a recursive function DRAW-LINE that draws a line of a specified length by doing (FORMAT T "*") the correct number of times.
+;;; (DRAW-LINE 10) should produce **********
+(defun draw-line (n)
+  (labels ((dl-helper (x result)
+             (cond ((>= x n) (mapcar #'(lambda (x) (format t x)) result))
+                   (t (dl-helper (+ x 1) (cons "*" result))))))
+    (dl-helper 0 nil)))
+
+(defun draw-line (n)
+  (cond ((zerop n) (format t "~%"))
+        (t (format t "*")
+           (draw-line (- n 1)))))
