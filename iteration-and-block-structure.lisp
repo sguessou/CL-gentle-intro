2db548559564d44fed128a1b53e026d9b3cd1269
Saad Guessous <guessous_saad@hotmail.com>
2019-01-07 21:40:17 +0200

    added Ex 11.3

--- iteration-and-block-structure.lisp
+++ iteration-and-block-structure.lisp
@@ -15,3 +15,18 @@
   (dolist (e x)
     (when (equal (car e) item)
       (return e))))
+
+;;; Ex 11.3
+;;; Write a recursive version of CHECK-ALL-ODD. It should produce the same message and the same result as the preceding iterative version.
+;;; Iterative version
+(defun check-all-odd (list-of-numbers)
+  (dolist (e list-of-numbers t)
+    (format t "~&Checking ~S..." e)
+    (if (not (oddp e)) (return nil))))
+
+;;; Recursive version
+(defun check-all-odd (x)
+  (cond ((null x) t)
+        ((not (oddp (car x))) nil)
+        (t (format t "~&Checking ~S..." (car x)) 
+           (check-all-odd (rest x)))))
