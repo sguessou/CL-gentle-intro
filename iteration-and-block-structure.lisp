937d1be2260f9b40e7f140f5c6376826b3b7ad07
Saad Guessous <guessous_saad@hotmail.com>
2019-01-07 21:26:10 +0200

    added Ex 11.2

--- iteration-and-block-structure.lisp
+++ iteration-and-block-structure.lisp
@@ -4,7 +4,14 @@
 ;;; Ex 11.1
 ;;; Write an iterative version of the MEMBER function, called IT-MEMBER. It should return T if its first input appears in its second input;
 ;;; It needs not return a sublist of its second input.
-(defun it-member (a b)
+(defun it-member (item b)
   (dolist (e b)
-    (if (equal a e)
+    (when (equal item e)
         (return t))))
+
+;;; Ex 11.2
+;;; Write an iterative version of ASSOC, called IT-ASSOC.
+(defun it-assoc (item x)
+  (dolist (e x)
+    (when (equal (car e) item)
+      (return e))))
