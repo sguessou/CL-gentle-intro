f0001f43e304363eb744a9b981935d3811ecad3d
Saad Guessous <guessous_saad@hotmail.com>
2019-01-17 06:45:30 +0200

    added Ex 12.4 d.

--- structures-type-system.lisp
+++ structures-type-system.lisp
@@ -18,15 +18,21 @@
 (setf *NODE-LIST* nil)
 
 (defun init ()
-  (setf *NODE-LIST* nil))
+  (setf *NODE-LIST* nil)
+  'initialized)
 
 ;;; c.
 ;;; Write ADD-NODE. It should return the name of the node it added.
-(defun add-node (s1 q1 q2 s2)
-  (make-node :name s1
-             :question q1
-             :yes-case q2
-             :no-case s2)
-  s1)
+(defun add-node (name question yes-case no-case)
+  (push (make-node :name name
+             :question question
+             :yes-case yes-case
+             :no-case no-case)
+        *NODE-LIST*)
+  name)
 
-
+;;; d. Write FIND-NODE, which takes a node name as input and returns the node if it appears in *NODE-LIST*, or NIL if it doesn't.
+(defun find-node (name)
+  (dolist (n *NODE-LIST*)
+    (when (equal name (node-name n))
+      (return n))))
