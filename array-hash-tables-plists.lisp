1d02e8b8087447cfd8b2dcfa39a7c3c1c2f60e84
Saad Guessous <guessous_saad@hotmail.com>
2019-01-20 11:07:46 +0200

    Added Ex 13.1

--- array-hash-tables-plists.lisp
+++ array-hash-tables-plists.lisp
@@ -2,4 +2,13 @@
 
 ;;; Exercises
 
-;;; 
+;;; Ex 13.1
+;;; Write a function called SUBPROP that deletes an element from a set stored under a property name. 
+;;; For example, if the symbol ALPHA has the list (A B C D E) as the value of its FOOPROP property, doing (SUBPROP 'ALPHA 'D 'FOOPROP) should leave (A B C E) as the value of ALPHA's FOOPROP property.
+(setf (get 'alpha 'fooprop) '(a b c d e))
+
+(defun subprop (name element prop)
+  (setf (get name prop)
+        (remove element
+                (get name prop))))
+
