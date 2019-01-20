094a64ae1015102cb6eb4d3b732396bb440fddba
Saad Guessous <guessous_saad@hotmail.com>
2019-01-20 16:28:51 +0200

    Added Ex 13.3

--- array-hash-tables-plists.lisp
+++ array-hash-tables-plists.lisp
@@ -12,3 +12,36 @@
         (remove element
                 (get name prop))))
 
+;;; Ex 13.2
+;;; Write a function called FORGET-MEETING that forgets that two particular persons have ever met each other.
+;;; Use SUBPROP in your solution.
+(defun addprop (sym elem prop)
+  (pushnew elem (get sym prop)))
+
+(defun record-meeting (x y)
+  (addprop x y 'has-met)
+  (addprop y x 'has-met)
+  t)
+
+(symbol-plist 'john)
+
+(defun forget-meeting (x y)
+  (remprop x 'has-met)
+  (remprop y 'has-met)
+  t)
+
+;;; Alternative solution
+(defun forget-meeting (person1 person2)
+  (subprop person1 person2 'has-met)
+  (subprop person2 person1 'has-met)
+  'forgotten)
+
+;;; Ex 13.3
+;;; Using SYMBOL-PLIST, write your own version of the GET function.
+(defun my-get (symbol property)
+  (do ((p (symbol-plist symbol) (cddr p)))
+      ((null p) nil)
+    (if (equal property (first p))
+        (return (second p)))))
+
+
